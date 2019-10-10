c5f5507e212b9a20d6e09cf0f7cc20fc9bd1db29
leahy089 <leahy089@morris.umn.edu>
2019-10-10 11:36:32 -0500

    Update echo client and server with basic socket implementation
    Currently not working
    
    Co-authored-by: UnicornWizard375 <drumm040@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,54 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoClient {
+    public static final int portNum = 6013;
+
+    public static void main(String[] args) throws IOException {
+        String server;
+
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        }
+        else {
+            server = args[0];
+        }
+
+
+        try {
+
+            InputStream input = System.in;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            Socket socket = new Socket(server, portNum);
+
+            OutputStream output = socket.getOutputStream();
+
+            int read = input.read();
+
+            while (read > 0) {
+                output.write(read);
+            }
+
+
+//            InputStream input = socket.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//
+//                System.out.println(line);
+//            }
+            socket.close();
+        }
+        catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,30 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+    public static final int portNum = 6013;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket sock = new ServerSocket(portNum);
+
+            while (true) {
+                Socket client = sock.accept();
+                System.out.println("Request accepted");
+
+                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+                //writer.println(new java.util.Date().toString());
+
+
+                client.close();
+            }
+        }
+        catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
